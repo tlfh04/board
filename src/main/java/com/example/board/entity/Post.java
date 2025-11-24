7db59f4d1c25f1a022c5a953c13a24211dd00cce
tlfh04 <126224780+tlfh04@users.noreply.github.com>
2025-11-24 11:39:01 +0900

    entity 구현

--- src/main/java/com/example/board/entity/Post.java
+++ src/main/java/com/example/board/entity/Post.java
@@ -2,6 +2,7 @@ package com.example.board.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
@@ -10,17 +11,27 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @Table(name="post")
+@NoArgsConstructor
 public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @Column
+    @Column(nullable = false,length = 100)
     private String title;
-    @Column
+    @Column(nullable = false,columnDefinition = "TEXT")
     private String content;
-    @Column
+    @Column(name = "create_at",updatable = false)
     private LocalDateTime createdAt;
 
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 
+    // 생성 시점에 자동으로 현재 생성 시간을 설정
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
